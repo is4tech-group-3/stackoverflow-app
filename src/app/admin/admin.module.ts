6b90343f26a4dadabee6c7eafe6f9c4823905294
Sebastian <sramirez-2022506@kinal.edu.gt>
2024-09-27 00:29:23 -0600

    Fixed bug and better look

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,7 +9,7 @@ import { DropdownModule } from 'primeng/dropdown';
 import { PaginatorModule } from 'primeng/paginator';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { MatOptionModule } from '@angular/material/core';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'; 
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { UsersComponent } from './users/users.component';
 import { UserModalComponent } from './users/user-modal/user-modal.component';
 import { AuditComponent } from './audit/audit.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 
 @NgModule({
@@ -32,6 +33,8 @@ declarations: [
     MenubarModule,
     MatTableModule,
     InputTextModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatPaginatorModule,
     FormsModule,
     PaginatorModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
-// shared.module.ts
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker'; // Importa el módulo correcto
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
+    CommonModule,
     MatMenuModule,
     MatButtonModule,
-    CommonModule,
-  ], exports: [NavbarComponent]
+    MatNativeDateModule,
+    MatDatepickerModule,  
+  ],
+  exports: [NavbarComponent]
 })
-
 export class SharedModule { }
