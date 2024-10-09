e52783613719efed58fe6a0bf046e69b93d53ca2
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-10-09 17:04:35 -0600

    Feature: functional unit test for formErrorService

--- src/app/shared/services/formError/form-error.service.spec.ts
+++ src/app/shared/services/formError/form-error.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FormErrorService } from './form-error.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 describe('FormErrorService', () => {
   let service: FormErrorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        TranslateModule.forRoot()  
+      ],
+      providers: [TranslateService]
+    });
     service = TestBed.inject(FormErrorService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
 });
